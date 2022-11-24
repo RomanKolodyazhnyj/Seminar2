0371ffa76a1ca1b1d7cbe971eacfc2f3061b4c8b
Roman <romek20@mail.ru>
2022-11-24 22:52:56 +0300

    Задания с 9 по 16

---
+++ Seminar_2/task 14/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 % number2 == 0)
+// {
+    // Console.WriteLine("Число кратное");
+// }
+// else
+// {
+    // Console.WriteLine("Остаток от деления: " + (number1 % number2));
+// }
+
+int num1 = 2;
+int num2 = 3;
+Console.WriteLine(num1 == 2 && num2 == 3);
---
+++ Seminar_2/task 15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// ​
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели числом: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+if (number == 6 || number == 7) 
+    Console.WriteLine("Выходной день");
+else Console.WriteLine("Будний день");
+}
+else
+    Console.WriteLine("Несуществующий день недели");
